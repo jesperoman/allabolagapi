4b7bdc4936239db0d7f8d92a1a1a3e2ee1a8afa2
Jesper Öman <jesper@mejsla.se>
2014-03-03 21:59:48 +0100

    First commit, some stuff up

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Allabolagapi::Application.config.secret_key_base = '37e49b1d60539433341fb55289c465b164f32f9c87a0868fe79fe5de4ab69936dad836142e0ba0c6f989bd6ae1722b66903bdcaf7b4066272d502a57e8547a07'
